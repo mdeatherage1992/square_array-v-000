5d6916ae3fad0f0243ab6ddb84f2249fe504a844
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-14 18:58:05 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,6 @@
 def square_array(array)
   new_array = []
-  counter = 0
   array.each do |number|
-  new_array.push(number**2)
-  counter += 1
-  return new_array
+    new_array.push(number**2)
   end
 end
